1f940a94e341a845fb409fdabb9508707b8b0a2b
Christian Flessa <chris@z0rn.net>
2026-06-15 21:55:59 +0200

    chore: remove redundant outcome tagging in OutboundTarget
    Signed-off-by: Christian Flessa <chris@z0rn.net>

--- src/Usf.Core/Messaging/OutboundTarget.cs
+++ src/Usf.Core/Messaging/OutboundTarget.cs
@@ -99,7 +99,6 @@ public abstract class OutboundTarget
         catch (OperationCanceledException)
         {
             outcome = "cancelled";
-            activity?.SetTag(OutboundDiagnostics.OutcomeTagName, outcome);
             throw;
         }
         catch (Exception exception)
