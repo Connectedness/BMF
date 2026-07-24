adbf6cf6c273f60b3ed0d5cc9a14d9ed57644798
Kenny Pflug <kenny.pflug@live.de>
2026-07-25 00:54:23 +0200

    chore: remove unused JetBrains annotations in BrilliantMessaging.GuardClauses.g.cs
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- src/BrilliantMessaging.Core/GuardClauses/BrilliantMessaging.GuardClauses.g.cs
+++ src/BrilliantMessaging.Core/GuardClauses/BrilliantMessaging.GuardClauses.g.cs
@@ -1,5 +1,5 @@
 /* ------------------------------
-   Source: Light.GuardClauses 15.1.0, commit f8581a1ac6abf6aee3b656061e1254a523e00571
+   Source: Light.GuardClauses 15.1.0, commit 7a52101f7280f2fca0fda7fd488fe929767b2e52
    Light.GuardClauses 15.1.0
    ------------------------------
 
@@ -46,7 +46,6 @@ using JetBrains.Annotations;
 using BrilliantMessaging.GuardClauses.Exceptions;
 using BrilliantMessaging.GuardClauses.ExceptionFactory;
 using BrilliantMessaging.GuardClauses.FrameworkExtensions;
-using NotNullAttribute = System.Diagnostics.CodeAnalysis.NotNullAttribute;
 
 #nullable enable annotations
 namespace BrilliantMessaging.GuardClauses
@@ -3199,81 +3198,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. */
 namespace JetBrains.Annotations
 {
-    /// <summary>
-    /// Indicates that the value of the marked element can never be <c>null</c>.
-    /// </summary>
-    /// <example><code>
-    /// [NotNull] object Foo() {
-    ///   return null; // Warning: Possible 'null' assignment
-    /// }
-    /// </code></example>
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.Delegate | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.GenericParameter)]
-    internal sealed class NotNullAttribute : Attribute
-    {
-    }
-
-    /// <summary>
-    /// Describes dependence between method input and output.
-    /// </summary>
-    /// <syntax>
-    /// <p>Function Definition Table syntax:</p>
-    /// <list>
-    /// <item>FDT      ::= FDTRow [;FDTRow]*</item>
-    /// <item>FDTRow   ::= Input =&gt; Output | Output &lt;= Input</item>
-    /// <item>Input    ::= ParameterName: Value [, Input]*</item>
-    /// <item>Output   ::= [ParameterName: Value]* {halt|stop|void|nothing|Value}</item>
-    /// <item>Value    ::= true | false | null | notnull | canbenull</item>
-    /// </list>
-    /// If the method has a single input parameter, its name could be omitted.<br/>
-    /// Using <c>halt</c> (or <c>void</c>/<c>nothing</c>, which is the same) for the method output
-    /// means that the method doesn't return normally (throws or terminates the process).<br/>
-    /// Value <c>canbenull</c> is only applicable for output parameters.<br/>
-    /// You can use multiple <c>[ContractAnnotation]</c> for each FDT row, or use single attribute
-    /// with rows separated by the semicolon. There is no notion of order rows, all rows are checked
-    /// for applicability and applied per each program state tracked by the analysis engine.<br/>
-    /// </syntax>
-    /// <examples><list>
-    /// <item><code>
-    /// [ContractAnnotation("=&gt; halt")]
-    /// public void TerminationMethod()
-    /// </code></item>
-    /// <item><code>
-    /// [ContractAnnotation("null &lt;= param:null")] // reverse condition syntax
-    /// public string GetName(string surname)
-    /// </code></item>
-    /// <item><code>
-    /// [ContractAnnotation("s:null =&gt; true")]
-    /// public bool IsNullOrEmpty(string s) // string.IsNullOrEmpty()
-    /// </code></item>
-    /// <item><code>
-    /// // A method that returns null if the parameter is null,
-    /// // and not null if the parameter is not null
-    /// [ContractAnnotation("null =&gt; null; notnull =&gt; notnull")]
-    /// public object Transform(object data)
-    /// </code></item>
-    /// <item><code>
-    /// [ContractAnnotation("=&gt; true, result: notnull; =&gt; false, result: null")]
-    /// public bool TryParse(string s, out Person result)
-    /// </code></item>
-    /// </list></examples>
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
-    internal sealed class ContractAnnotationAttribute : Attribute
-    {
-        public ContractAnnotationAttribute([NotNull] string contract) : this(contract, false)
-        {
-        }
-
-        public ContractAnnotationAttribute([NotNull] string contract, bool forceFullStates)
-        {
-            Contract = contract;
-            ForceFullStates = forceFullStates;
-        }
-
-        [NotNull]
-        public string Contract { get; }
-        public bool ForceFullStates { get; }
-    }
-
     /// <summary>
     /// Indicates that IEnumerable passed as a parameter is not enumerated.
     /// Use this annotation to suppress the 'Possible multiple enumeration of IEnumerable' inspection.
